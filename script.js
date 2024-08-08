777820a9ecff520394006a114427fd31d4a0220a
Bruna Morais <brunnalarissa200@gmail.com>
2024-08-08 00:41:09 -0300

    finalizado dark mode

---
+++ script.js
@@ -0,0 +1,15 @@
+// function toggleMode(){
+//     const html = document.documentElement
+//     //toggle se tiver o light ele tira, se não tiver ele adiciona
+//     html.classList.toggle('light')
+
+//     const img = document.querySelector("#profile img")
+//     if(html.classList.contains('light')){
+//         img.setAttribute('src', './assets/avatar-light.png')
+//         img.setAttribute('alt', 'img 1')
+
+//     } else {
+//         img.setAttribute('src', './assets/avatar.png')
+//         img.setAttribute('alt', 'img 2')
+//     }
+//}
